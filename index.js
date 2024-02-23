05151f5330ee33fd2833363a6a26a3d4b58cc1c9
Nnamdi Orie <nnamdi.orie@iisysgroup.com>
2024-02-23 16:57:33 +0100

    Second commit

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ app.use(express.urlencoded({extended:true}))
 app.get("/", (req,res) => {
     res.send("App is live on cyclic")
 })
+
+app.get("/api/v1", (req,res)=>{
+    console.log("Logging on terminal")
+    res.send("API ready for consumption")
+})
 const port = process.env.PORT || 3000
 app.listen(port, ()=>console.log(`App is listening on port ${port}`))
